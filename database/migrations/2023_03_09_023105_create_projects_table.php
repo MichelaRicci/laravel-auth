dfebb5d156ef7cc4365ba98781a22835da775ffc
MichelaRicci <michela.ricci.1991@gmail.com>
2023-03-09 14:24:03 +0100

    cruds, migrations added

--- database/migrations/2023_03_09_023105_create_projects_table.php
+++ database/migrations/2023_03_09_023105_create_projects_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50)->unique();
+            $table->text('content')->nullable();
+            $table->string('slug');
             $table->timestamps();
         });
     }
