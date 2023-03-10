1c0adf2003f0dfbc224715670d0e000ac926a91d
MichelaRicci <michela.ricci.1991@gmail.com>
2023-03-10 18:44:11 +0100

    destroy added

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -5,18 +5,27 @@
 @section('content')
 <section id="projects">
     <div class="container py-4">
-        <div class="d-flex g-3 flex-wrap">
+        <div class="d-flex flex-wrap">
             @foreach ($projects as $project)
-                <div class="col-6 my-3" style="height: 300px;">
+                <div class="col-6 my-3" style="height: 400px;">
                     <a href="{{ route('admin.projects.show', $project['id']) }}" style="text-decoration: none; color:black">
-                        <div class="card d-flex flex-column-reverse align-items-center justify-content-between h-100">                     
-                            <figure class="text-center w-100 h-50 ">
-                                <img src="{{ $project->image }}" alt="{{ $project->title }}" class="img-fluid h-100">
+                        <div class="card d-flex flex-column-reverse align-items-center justify-content-between mb-3">                     
+                            <figure class="text-center">
+                                <img src="{{ $project->image }}" alt="{{ $project->title }}">
                             </figure>
                             <div class="text-center">
                                 <h1>Project {{ $project->title }}</h1>
                             </div>
                         </div>
+                    </a>
+                    <div class="d-flex justify-content-center align-items-center mt-3 mb-5 gap-3">                     
+                        <a class="btn btn-warning" href="{{ route('admin.projects.edit', $project->id) }}">Edit</a>
+                        <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button class="btn btn-danger" type="submit">Delete</button>
+                        </form>
+                    </div>
                 </div>
             @endforeach
         </div>
